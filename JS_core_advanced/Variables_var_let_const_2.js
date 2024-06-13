8df941444c72b55312703c34436ce1fd61b3772d
KateAnufrieva <93079863+KateAnufrieva@users.noreply.github.com>
2024-06-13 12:39:24 +0300

    Add files via upload

---
+++ JS_core_advanced/Variables_var_let_const_2.js
@@ -0,0 +1,22 @@
+function variablesFunction(){
+    var x = 'var';
+    let y = 'let';
+    const z = 0;
+
+    console.log(x, y, z);
+    console.log(x1); 
+    console.log(y1); 
+    console.log(z1); 
+
+}
+
+var x1 = 'var_';
+let y1 = 'let_';
+const z1 = 1;
+
+variablesFunction();
+console.log(x1, y1, z1);
+
+//console.log(x); x is not defined
+//console.log(y); y is not defined
+//console.log(z); z is not defined
